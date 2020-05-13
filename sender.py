2b30525e6a8dc3275b7bd388da51f3d4db751dae
Pietro Manzoni <manzoni@ieee.org>
2020-05-13 19:23:44 +0200

    Deleted one example

--- sender.py
+++ sender.py
@@ -29,12 +29,8 @@ send_it(tbs)
 tbs = b'1'
 send_it(tbs)
 
-tbs = b'\x06'   # ASCII ACK
-send_it(tbs)
-
 tbs = b'Hi, this is a msg from pietro and by the way is exactly 202 bytes long which is the size of a packet in the loractp library. I repeat, this is a msg from pietro and by the way is exactly 202 bytes long.'
 send_it(tbs)
 
 tbs = b"Ciao, questo è un messaggio da pietro ed è esattamente lungo 226 byte, che è maggiore della dimensione di un pacchetto nella libreria loractp. Ripeto, questo è un messaggio da pietro e tra l'altro è lungo esattamente 226 byte."
 send_it(tbs)
-
