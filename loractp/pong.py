b4019d5a26ad1a0b8fde75f6c39eed19ad480702
pmanzoni <manzoni@ieee.org>
2020-11-03 17:38:17 +0100

    New version after testing for MSN 2020 paper

--- loractp/pong.py
+++ loractp/pong.py
@@ -1,14 +1,16 @@
-import time
-import loractp
+# MicroPython
 
 import ujson
-import ucrypto
 import math
+import time
+import gc
+import ucrypto
+import loractp
+
 def random_in_range(l=0,h=1000):
     r1 = ucrypto.getrandbits(32)
     r2 = ((r1[0]<<24)+(r1[1]<<16)+(r1[2]<<8)+r1[3])/4294967295.0
     return math.floor(r2*h+l)
-
 
 ctpc = loractp.CTPendpoint()
 
@@ -28,13 +30,20 @@ while True:
         print ("pong.py: EXCEPTION!! ", e)
         break
 
-    tbs = {"type": "PONG", "value": random_in_range(), "time": time.time()}
+    tbs = {"type": "PONG", "value": "ADIOS", "time": time.time()}
     tbsj = ujson.dumps(tbs)
     tbsb = str.encode(tbsj)
-    print('pong.py: sending ', tbsb)
+    print('--->pong.py: sending ', tbsb)
     try:
         addr, quality, result = ctpc.sendit(rcvraddr, tbsb)
         print("pong.py: ACK from {} (quality = {}, result = {})".format(addr, quality, result))
     except Exception as e:
         print ("pong.py: EXCEPTION!! ", e)
         break
+
+    # Enabling garbage collection
+    gc.enable()
+    gc.collect()
+    tbs = ""
+    tbsj = ""
+    tbsb = ""
